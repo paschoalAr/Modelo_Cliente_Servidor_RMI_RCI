b7362d7b196fa6e694e24086f968ccafe9a6d127
paschoalAr <arthur.paschoal@edu.pucrs.br>
2024-10-31 10:51:48 -0300

    Implementação das funções da AgenciaCliente

--- ContasInterface.java
+++ ContasInterface.java
@@ -3,7 +3,12 @@ import java.rmi.RemoteException;
 
 
 public interface ContasInterface extends Remote{
+    // funções de uso dos dois clietes (CaixaAutoCliente e AgenciaCliente)
     public double saldo(int id) throws RemoteException;
     public void deposito(int id, double valor) throws RemoteException;
     public void saque(int id, double valor) throws RemoteException;
+
+    // funções de uso do cliente AgenciaCliente
+    public void criaConta(int id, String nome, double saldo) throws RemoteException;
+    public void removeConta(int id) throws RemoteException;
 }
